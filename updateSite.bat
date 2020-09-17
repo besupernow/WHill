288ccc417e1485d98126468bce5646b47c1f3def
Bryson Marshall <bryson.marshall@stu.marshall.kyschools.us>
2020-09-17 08:30:11 -0500

    Testing

--- updateSite.bat
+++ updateSite.bat
@@ -1,7 +1,10 @@
-@echo off
 git add .
+echo Files Added
 echo Please Input the Commit Name
 set INPUT=
 set /P INPUT=Type input: %=%
 git commit -m "%INPUT%"
+echo Committed %INPUT%
 git push
+echo Files Finshed Uploading
+pause
